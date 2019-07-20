b19f0cab71d0c093b09052d4fc435809389e692d
luojianrong <luojianrong00@gmail.com>
2019-07-20 11:02:10 +0800

    更新了新的内容

---
+++ ES6/模块化/index.js
@@ -0,0 +1,12 @@
+const module2 = require("./module2.js");
+const module1 = require("./module1.js");
+const http = require("http");
+
+
+console.log(module1.add(1, 3));;
+console.log(module1.cum(2, 3));;
+
+console.log(module2(4, 3));;
+
+
+console.log(http);
---
+++ ES6/模块化/module1.js
@@ -0,0 +1,10 @@
+function add(a,b) {
+  return a+b;
+}
+
+function cum(a,b) {
+  return a*b;
+}
+
+exports.add = add ;
+exports.cum = cum;
